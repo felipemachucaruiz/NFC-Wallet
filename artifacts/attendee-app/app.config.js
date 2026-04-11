450ba44a4056d1425218213f8d7d103786cbc625
felipemachucaruiz <72819525+felipemachucaruiz@users.noreply.github.com>
2026-04-11 17:59:44 +0000

    Bump attendee buildNumber to 13

--- artifacts/attendee-app/app.config.js
+++ artifacts/attendee-app/app.config.js
@@ -17,7 +17,7 @@ module.exports = {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.tapee.attendee",
-      buildNumber: "12",
+      buildNumber: "13",
       infoPlist: {
         NFCReaderUsageDescription: "Used to read NFC wristbands for cashless payments.",
         ITSAppUsesNonExemptEncryption: false,
